592e5f98d99d14d97132b9c42c7d3d00cc5fcf4d
Anton Stecyura <grandwar39@gmail.com>
2022-11-25 21:39:58 +0200

    Move model data to json folder.

--- client/src/store/slices/model.ts
+++ client/src/store/slices/model.ts
@@ -1,74 +1,21 @@
-import { IDType, Worker } from './../../types/index';
+import { IDType } from './../../types/index';
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
-import { BuildingModel } from '../../types/model';
+import { BuildingModel, Floor, Worker } from '../../types/three';
+import workers from '../../json/workers.json';
+import floors from '../../json/floors.json';
+
 
 interface ModelInitialState {
     model: BuildingModel;
     selectedFloor: number | null;
     workers: Worker[]
 }
-
 const initialState: ModelInitialState = {
     model: {
-        floors: [
-            {id: '1', height: 10, shape: {
-                shapeCenterPoint: [5, 5],
-                points: [
-                    {type: 'straight', coordinate: [0, 0]},
-                    {type: 'straight', coordinate: [0, 7]},
-                    {type: 'straight', coordinate: [7, 7]},
-                    {type: 'circle', coordinate: [7, 7], radius: 6},
-                    {type: 'straight', coordinate: [7, 0]},
-                ]
-            }},
-            {id: '2', height: 5, shape: {
-                shapeCenterPoint: [3, 3],
-                points: [
-                    {type: 'straight', coordinate: [0, 0]},
-                    {type: 'straight', coordinate: [0, 6]},
-                    {type: 'straight', coordinate: [6, 6]},
-                    {type: 'circle', coordinate: [7, 7], radius: 5},
-                    {type: 'straight', coordinate: [6, 0]},
-                ]
-            }},
-            {id: '3', height: 5, shape: {
-                shapeCenterPoint: [3, 3],
-                points: [
-                    {type: 'straight', coordinate: [0, 0]},
-                    {type: 'straight', coordinate: [0, 6]},
-                    {type: 'straight', coordinate: [6, 6]},
-                    {type: 'circle', coordinate: [7, 7], radius: 5},
-                    {type: 'straight', coordinate: [6, 0]},
-                ]
-            }},
-             {id: '4', height: 5, shape: {
-                shapeCenterPoint: [3, 3],
-                points: [
-                    {type: 'straight', coordinate: [0, 0]},
-                    {type: 'straight', coordinate: [0, 6]},
-                    {type: 'straight', coordinate: [6, 6]},
-                    {type: 'circle', coordinate: [7, 7], radius: 5},
-                    {type: 'straight', coordinate: [6, 0]},
-                ]
-            }},
-            {id: '5', height: 4, shape: {
-                shapeCenterPoint: [5, 5],
-                points: [
-                    {type: 'straight', coordinate: [0, 0]},
-                    {type: 'straight', coordinate: [0, 7]},
-                    {type: 'straight', coordinate: [7, 7]},
-                    {type: 'circle', coordinate: [7, 7], radius: 6},
-                    {type: 'straight', coordinate: [7, 0]},
-                ]
-            }},
-        ],
+        floors: floors as Floor[],
     },
     selectedFloor: null,
-    workers: [
-        {id: '1', coordinates: [19, 19], floor: 2, tile: {color: 'blue'}, name: 'Anton', age: 17},
-        // {id: '2', coordinates: [19, 19], floor: 2, tile: {color: 'green'}, name: 'Ignat', age: 18},
-        {id: '3', coordinates: [0, 0], floor: 3, tile: {color: 'red'}, name: 'Oleh', age: 38},
-    ]
+    workers: workers as Worker[],
 };
 
 export const modelSlice = createSlice({
